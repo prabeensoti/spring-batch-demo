8a9f4240987fb6e45ca1e52dd9b99fdc4301745b
PRABEEN SOTI <prabeen.soti@hotmail.com>
2022-09-06 16:06:33 -0500

    Project Init

---
+++ src/main/java/org/miu/cs590/springbatchdemo/SpringBatchDemoApplication.java
@@ -0,0 +1,13 @@
+package org.miu.cs590.springbatchdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/miu/cs590/springbatchdemo/SpringBatchDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.miu.cs590.springbatchdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
